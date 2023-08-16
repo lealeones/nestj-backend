04d5bc25a2375b23c006a389cbd351f471348679
lealeones <leandrooleones@gmail.com>
2023-08-16 00:24:07 +0000

    feat: first response query createUser

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
+
 import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
